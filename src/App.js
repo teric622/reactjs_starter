a5238fbbafd885ef286d39bce42faee32a64dc4d
teric622 <tlasecaeric@gmail.com>
2020-05-08 17:41:10 +0000

    installing nav

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/LoginContainer/Login.js";
 import SignUp from "./components/SignUpContainer/SignUp.js";
 import WelcomeScreen from "./components/WelcomeContainer/WelcomeScreen.js";
+import GameStats from "./components/GameStatsContainer/GameStats.js";
+import Donate from "./components/DonateContainer/Donate.js";
 
 function App() {
   return (<Router>
@@ -16,10 +18,20 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                <Link className="nav-link" to={"/welcome-screen"}>Welcome</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Log
+                in</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+               <li className="nav-item">
+                <Link className="nav-link" to={"/game-stats"}>Game stats</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/donate"}>Donate</Link>
               </li>
             </ul>
           </div>
@@ -30,8 +42,11 @@ function App() {
         <div className="auth-inner">
           <Switch>
             <Route exact path='/' component={Login} />
+             <Route path="/welcome-screen" component={WelcomeScreen} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
+            <Route path="/game-stats" component={GameStats} />
+            <Route path="/donate" component={Donate}/>
           </Switch>
         </div>
       </div>
